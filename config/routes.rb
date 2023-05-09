1c886e7166801b290215728fb3997fe6edd2353e
ZeroooxY <abiyan@happy5.co>
2023-05-09 17:26:08 +0700

    add Conversation Routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'conversations/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :conversations, only: [:index, :create] do
-    resources :messages, only: [:index, :create]
+    resources :messages, only: [:index, :create] 
   end
 end
